599513ff27cd055045a9e15222a127248ee7f9ab
aswma317 <aswma317@student.liu.se>
2022-09-24 16:18:14 +0200

    Included line graph

--- KoladaVisual_Shiny/app.R
+++ KoladaVisual_Shiny/app.R
@@ -12,9 +12,11 @@ library(httr)
 library(jsonlite)
 library(tidyr)
 library(KoladaPackage)
-data_frame <- get_kolda_data('kpi/n60026/year/2020,2019,2018')
-data_frame
+library(ggplot2)
+library(scales)
 # Define UI for application that draws a histogram
+data_frame <- get_kolda_data('kpi/n60026/year/2020,2019,2018,2017,2015,2016') 
+data_frame <- cbind(data_frame[,1:5], value = as.integer(data_frame[,7]))
 ui <- fluidPage(
 
     # Application title
@@ -34,8 +36,7 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-          textOutput(outputId = 'chooseKPI'),
-          textOutput(outputId = 'chooseMunici'),
+          textOutput("municipality"),
           plotOutput('plot1', width = "700px", height = '500px')
         )
     )
@@ -44,15 +45,21 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$chooseKPI <- renderText({
-      chooseKPI <- print(input$chooseKPI)
-    })
-    output$chooseMunici <- renderText({
-      chooseMunici <- print(input$chooseMunici)
-    })
     output$plot1 <- renderPlot({
-      plot(x = data_frame$period, y = data_frame$value)
+      if(input$chooseMunici != ""){
+        df <- data_frame[data_frame['kpi'] == input$chooseKPI &
+                           data_frame['municipality'] == input$chooseMunici,]
+        ggplot(data = df, 
+               aes(x=period, y=value, color = gender)) + 
+          geom_line() +
+          geom_point() +
+          scale_y_continuous(breaks = pretty_breaks())
+      }
+      
     })
+    output$municipality <- {(
+      renderText(input$chooseMunici)
+    )}
 
 }
 
